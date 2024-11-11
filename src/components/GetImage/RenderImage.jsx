bb91f1c3ea8f839f86953ea5f2487ca5a16c1948
Shahnawaz Khan <1990.shahnawazkhan@gmail.com>
2024-11-11 23:16:10 +0530

    added image title

--- src/components/GetImage/RenderImage.jsx
+++ src/components/GetImage/RenderImage.jsx
@@ -3,11 +3,18 @@ import Header from "../Header/Header";
 import styles from "./GetImage.module.css";
 
 function RenderImage() {
-  const [caption, setCaption] = React.useState("Add image Caption here!");
+  const [caption, setCaption] = React.useState("Image caption");
+  const [imageTitle, setImageTitle] = React.useState("Title");
+
   function handleAddCaption() {
     setCaption(txtAreaCaption.value);
     txtAreaCaption.value = "";
   }
+
+  function handleChange(event) {
+    setImageTitle(event.target.value);
+  }
+
   return (
     <>
       <Header />
@@ -20,17 +27,28 @@ function RenderImage() {
               width={"100%"}
               height={"500px"}
             />
-            <figcaption>{caption}</figcaption>
+            <figcaption>
+              <h4>{imageTitle}</h4>
+              {caption}
+            </figcaption>
           </figure>
         </div>
         <div className={styles.rightContainer}>
           <div className={styles.w100}>
+            <input
+              type="text"
+              name="txtImageTitle"
+              id="txtImageTitle"
+              className={styles.txtImageTitle}
+              placeholder="Add Image Title..."
+              onChange={handleChange}
+            />
             <textarea name="txtAreaCaption" id="txtAreaCaption"></textarea>
             <button
               className={`${styles.button} ${styles.btnCaption}`}
               onClick={handleAddCaption}
             >
-              Add Caption
+              Add Image Caption
             </button>
           </div>
           <button className={`${styles.button} ${styles.btnImage}`}>
